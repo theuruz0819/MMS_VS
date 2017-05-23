090c17ce836d4d4bb3b7fca592687502b7ce441a
Balder <Balder@BALDER-PC>
2017-05-24 07:17:43 +0800

    加入專案檔案。

---
+++ MMS/MMG.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMS
+{
+    class MMG
+    {
+        String mmgFloderPath;
+        String processFloderPath;
+        String storageFloderPath;
+
+        String storagePathAlphabet;
+        String storagePathOthers;
+        String storagePathLineA;
+        String storagePathLineKA;
+        String storagePathLineSA;
+        String storagePathLineTA;
+        String storagePathLineNA;
+        String storagePathLineHA;
+        String storagePathLineMA;
+        String storagePathLineYA;
+
+
+        public void initital() {
+            mmgFloderPath = ConfigurationManager.AppSettings["folder_path"];
+            processFloderPath = mmgFloderPath + "\\process";
+            storageFloderPath = mmgFloderPath + "\\storage";
+
+            storagePathAlphabet = storageFloderPath + "\\A_Z";
+            storagePathOthers = storageFloderPath + "\\others";
+            storagePathLineA = storageFloderPath + "\\line_あ";
+            storagePathLineKA = storageFloderPath + "\\line_か";
+            storagePathLineSA = storageFloderPath + "\\line_さ";
+            storagePathLineTA = storageFloderPath + "\\line_た";
+            storagePathLineNA = storageFloderPath + "\\line_な";
+            storagePathLineHA = storageFloderPath + "\\line_は";
+            storagePathLineMA = storageFloderPath + "\\line_ま";
+            storagePathLineYA = storageFloderPath + "\\line_や";
+
+            System.IO.Directory.CreateDirectory(processFloderPath);
+            System.IO.Directory.CreateDirectory(storageFloderPath);
+
+            System.IO.Directory.CreateDirectory(storagePathAlphabet);
+            System.IO.Directory.CreateDirectory(storagePathOthers);
+            System.IO.Directory.CreateDirectory(storagePathLineA);
+            System.IO.Directory.CreateDirectory(storagePathLineKA);
+            System.IO.Directory.CreateDirectory(storagePathLineSA);
+            System.IO.Directory.CreateDirectory(storagePathLineTA);
+            System.IO.Directory.CreateDirectory(storagePathLineNA);
+            System.IO.Directory.CreateDirectory(storagePathLineHA);
+            System.IO.Directory.CreateDirectory(storagePathLineMA);
+            System.IO.Directory.CreateDirectory(storagePathLineYA);
+
+
+            var files = System.IO.Directory.GetDirectories(mmgFloderPath, "*", SearchOption.TopDirectoryOnly);
+            foreach (String str in files){
+                Console.WriteLine(Path.GetFileName((str)));
+            }
+            
+        }
+        
+        Boolean isMangaStorageFloders(String dirName) {
+
+            if (dirName.Contains("mSF"))
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public void processFloder() {
+            var files = System.IO.Directory.GetDirectories(processFloderPath, "*", SearchOption.TopDirectoryOnly);
+            foreach (String str in files)
+            {
+                var startIndex = str.IndexOf("[");
+                var endIndex = str.IndexOf("]");
+                var authorName = str.Substring(startIndex, endIndex - startIndex + 1);
+                int initial = authorName[1];
+
+                Console.WriteLine(Path.GetFileName((str)));
+                Console.WriteLine(authorName);
+                Console.WriteLine(initial);
+
+                var targetPath = storagePathOthers;
+
+                if (initial >= 12353 & initial <= 12362)
+                {
+                    targetPath = storagePathLineA;
+                }
+                else if (initial >= 12363 & initial <= 12372)
+                {
+                    targetPath = storagePathLineKA;
+                }
+                else if (initial >= 12373 & initial <= 12382)
+                {
+                    targetPath = storagePathLineSA;
+                }
+                else if (initial >= 12383 & initial <= 12393)
+                {
+                    targetPath = storagePathLineTA;
+                }
+                else if (initial >= 12394 & initial <= 12398)
+                {
+                    targetPath = storagePathLineNA;
+                }
+                else if (initial >= 12399 & initial <= 12413)
+                {
+                    targetPath = storagePathLineHA;
+                }
+                else if (initial >= 12414 & initial <= 12418)
+                {
+                    targetPath = storagePathLineMA;
+                }
+                else if (initial >= 12419 & initial <= 12447)
+                {
+                    targetPath = storagePathLineYA;
+                }
+                else if (initial >= 65 & initial <= 90)
+                {
+                    targetPath = storagePathAlphabet;
+                }
+                else if (initial >= 97 & initial <= 122)
+                {
+                    targetPath = storagePathAlphabet;
+                }
+
+                Console.WriteLine(targetPath + "\\" + authorName);
+                
+                
+                System.IO.Directory.CreateDirectory(@targetPath + "\\" + authorName);
+                try
+                {
+                    System.IO.Directory.Move(@str, @targetPath + "\\" + authorName + "\\" + Path.GetFileName((str)));
+                }
+                catch {
+                }
+                
+            }
+        }
+
+    }
+}
---
+++ MMS/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Windows;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Configuration;
+namespace MMS
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MMG mmg = new MMG();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            string value = ConfigurationManager.AppSettings["folder_path"];
+            folder_select_text_box.Text = value;
+        }
+
+        private void folder_select_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            var path  = dialog.SelectedPath.ToString();
+            //ConfigurationManager.AppSettings["folder_path"] = result.ToString();
+
+            var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configFile.AppSettings.Settings;
+            if (settings["folder_path"] == null)
+            {
+                settings.Add("folder_path", path);
+            }
+            else
+            {
+                settings["folder_path"].Value = path;
+            }
+            configFile.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+
+            folder_select_text_box.Text = ConfigurationManager.AppSettings["folder_path"];
+
+        }
+
+        private void start_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configFile.AppSettings.Settings;
+            if (settings["folder_path"] == null)
+            {
+                settings.Add("folder_path", folder_select_text_box.Text);
+            }
+            else
+            {
+                settings["folder_path"].Value = folder_select_text_box.Text;
+            }
+            configFile.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+
+            mmg.initital();
+        }
+
+        private void process_btn_Click(object sender, RoutedEventArgs e)
+        {
+            mmg.processFloder();
+        }
+
+        private void folder_select_text_box_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
